f6d69e9ceb5de19fa48622adf51186519191c586
Henry Thomas <henrykun95@gmail.com>
2025-05-09 22:02:12 +0100

    toggle about section

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+// navbar select
+document.addEventListener('DOMContentLoaded', selectLink);
+
+function selectLink() {
+  const navLinks = document.querySelectorAll('.nav-link');
+
+  navLinks.forEach((link) => {
+    link.addEventListener('click', () => {
+      // remove 'active' from each link
+      navLinks.forEach(l => l.classList.remove('active'));
+      // add only to one link
+      link.classList.add('active');
+    });
+  });
+};
+
+// projects slide
 let slideIndex = 1;
 showDivs(slideIndex);
 
@@ -16,18 +33,22 @@ function showDivs(n) {
   slides[slideIndex-1].style.display = "block";
 }
 
-// navbar select
-document.addEventListener('DOMContentLoaded', selectLink);
+// about toggle
+const expRadio = document.getElementById('experience');
+const intRadio = document.getElementById('interests');
+const expCard = document.getElementById('experience-card');
+const intCard = document.getElementById('interests-card');
+const radioButtons = document.querySelectorAll('.radio');
 
-function selectLink() {
-  const navLinks = document.querySelectorAll('.nav-link');
-
-  navLinks.forEach((link) => {
-    link.addEventListener('click', () => {
-      // remove 'active' from each link
-      navLinks.forEach(l => l.classList.remove('active'));
-      // add only to one link
-      link.classList.add('active');
-    });
-  });
-};
+radioButtons.forEach((radio) => {
+  radio.addEventListener('click', () => {
+    if (expRadio.checked) {
+      expCard.style.display = 'block';
+      intCard.style.display = 'none';
+    }
+    else if (intRadio.checked) {
+      intCard.style.display = 'block';
+      expCard.style.display = 'none';
+    }
+  })
+})

3bdf69896169952102a765516cbe2bd26c744f0b
Henry Thomas <henrykun95@gmail.com>
2025-05-15 22:59:18 +0100

    added focus to skills section

--- script.js
+++ script.js
@@ -33,40 +33,96 @@ function showDivs(n) {
   slides[slideIndex-1].style.display = "block";
 }
 
-// about toggle
-const radioButtons = document.querySelectorAll('.radio');
+// about and skills toggle
+const aboutRadioButtons = document.querySelectorAll('.radio');
+const skillsRadioButtons = document.querySelectorAll('.skills-radio');
 
-const radio = {
+const radioButtons = {
   experience: document.getElementById('experience'),
   interests: document.getElementById('interests'),
-  education: document.getElementById('education')
-}
+  education: document.getElementById('education'),
+  languages: document.getElementById('languages'),
+  frameworks: document.getElementById('frameworks'),
+  tools: document.getElementById('tools'),
+  databases: document.getElementById('databases')
+};
 
 const card = {
   experience: document.getElementById('experience-card'),
   interests: document.getElementById('interests-card'),
   education: document.getElementById('education-card')
-}
+};
 
-radioButtons.forEach((button) => {
+const languageIcons = [
+  document.getElementById('ruby'),
+  document.getElementById('js'),
+  document.getElementById('html'),
+  document.getElementById('css'),
+  document.getElementById('ts')
+];
+
+const frameworkIcons = [
+  document.getElementById('rails'),
+  document.getElementById('bootstrap'),
+];
+
+const toolIcons = [
+  document.getElementById('git'),
+  document.getElementById('github'),
+  document.getElementById('heroku'),
+];
+
+const dbIcons = [
+  document.getElementById('postgres')
+];
+
+
+aboutRadioButtons.forEach((button) => {
   button.addEventListener('click', () => {
-    if (radio.experience.checked) {
+    if (radioButtons.experience.checked) {
       card.experience.style.display = 'block';
       card.interests.style.display = 'none';
       card.education.style.display = 'none';
     }
-    else if (radio.interests.checked) {
+    else if (radioButtons.interests.checked) {
       card.interests.style.display = 'block';
       card.experience.style.display = 'none';
       card.education.style.display = 'none';
     }
-     else if (radio.education.checked) {
+     else if (radioButtons.education.checked) {
       card.education.style.display = 'block';
       card.experience.style.display = 'none';
       card.interests.style.display = 'none';
     }
   })
 })
+
+function clearFade() {
+  [...languageIcons, ...frameworkIcons, ...toolIcons, ...dbIcons].forEach((icon) => icon.classList.remove('fade'));
+}
+
+skillsRadioButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    clearFade();
+    if (radioButtons.languages.checked) {
+      frameworkIcons.forEach((icon) => icon.classList.add('fade'));
+      toolIcons.forEach((icon) => icon.classList.add('fade'));
+      dbIcons.forEach((icon) => icon.classList.add('fade'));
+    } else if (radioButtons.frameworks.checked) {
+      languageIcons.forEach((icon) => icon.classList.add('fade'));
+      toolIcons.forEach((icon) => icon.classList.add('fade'));
+      dbIcons.forEach((icon) => icon.classList.add('fade'));
+    } else if (radioButtons.tools.checked) {
+      languageIcons.forEach((icon) => icon.classList.add('fade'));
+      frameworkIcons.forEach((icon) => icon.classList.add('fade'));
+      dbIcons.forEach((icon) => icon.classList.add('fade'));
+    } else if (radioButtons.databases.checked) {
+      languageIcons.forEach((icon) => icon.classList.add('fade'));
+      frameworkIcons.forEach((icon) => icon.classList.add('fade'));
+      toolIcons.forEach((icon) => icon.classList.add('fade'));
+    }
+  });
+});
 
 // petal moving
 const interval = setInterval(movePetal(), 1000);
@@ -77,4 +133,13 @@ function movePetal() {
   petal.style.left = petal.offsetLeft + 1 + "px";
 };
 
-// canvas
+// music album flip
+const albumCards = document.querySelectorAll(".flip-album");
+
+function flipCard() {
+  this.classList.toggle("flip");
+}
+
+albumCards.forEach((card) => {
+  card.addEventListener("click", flipCard);
+});

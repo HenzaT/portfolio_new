7710073bd84558726165be2f6e88a8def3416eb4
HenzaT <henrykun95@gmail.com>
2026-04-13 15:46:39 +0100

    edited config file

---
+++ dist/skills/skillsData.js
@@ -0,0 +1,101 @@
+export const skillsData = [
+    {
+        'id': 'ruby',
+        'class': 'devicon-ruby-plain',
+        'title': 'ruby icon',
+    },
+    {
+        'id': 'ts',
+        'class': 'devicon-typescript-plain',
+        'title': 'typescript icon',
+    },
+    {
+        'id': 'js',
+        'class': 'devicon-javascript-plain',
+        'title': 'javascript icon',
+    },
+    { 'id': 'python',
+        'class': 'devicon-python-plain',
+        'title': 'python icon',
+    },
+    {
+        'id': 'perl',
+        'class': 'devicon-perl-plain',
+        'title': 'perl icon',
+    },
+    {
+        'id': 'rails',
+        'class': 'devicon-rails-plain',
+        'title': 'ruby on rails icon',
+    },
+    {
+        'id': 'bootstrap',
+        'class': 'devicon-bootstrap-plain',
+        'title': 'bootstrap icon',
+    },
+    {
+        'id': 'react',
+        'class': 'devicon-react-original',
+        'title': 'reactjs icon',
+    },
+    {
+        'id': 'html',
+        'class': 'devicon-html5-plain-wordmark',
+        'title': 'html5 icon',
+    },
+    {
+        'id': 'css',
+        'class': 'devicon-css3-plain-wordmark',
+        'title': 'css3 icon',
+    },
+    {
+        'id': 'sass',
+        'class': 'devicon-sass-original',
+        'title': 'sass icon',
+    },
+    {
+        'id': 'git',
+        'class': 'devicon-git-plain-wordmark',
+        'title': 'git icon',
+    },
+    {
+        'id': 'github',
+        'class': 'devicon-github-original',
+        'title': 'github icon',
+    },
+    {
+        'id': 'heroku',
+        'class': 'devicon-heroku-original',
+        'title': 'heroku icon',
+    },
+    {
+        'id': 'netlify',
+        'class': 'devicon-netlify-plain',
+        'title': 'netlify icon',
+    },
+    {
+        'id': 'linux',
+        'class': 'devicon-linux-plain',
+        'title': 'linux icon',
+    },
+    {
+        'id': 'proxmox',
+        'class': 'devicon-proxmox-plain',
+        'title': 'proxmox icon',
+    },
+    {
+        'id': 'postgres',
+        'class': 'devicon-postgresql-plain',
+        'title': 'postgres icon',
+    },
+    {
+        'id': 'mysql',
+        'class': 'devicon-mysql-original',
+        'title': 'mysql icon',
+    },
+    {
+        'id': 'mariadb',
+        'class': 'devicon-mariadb-original-wordmark',
+        'title': 'mariadb icon',
+    },
+];
